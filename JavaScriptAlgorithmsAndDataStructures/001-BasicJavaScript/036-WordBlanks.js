b678a07b573e8b9f8e2b5ca247eafcf867b90612
Cassius Cives <cass-cives@hotmail.com>
2021-02-22 16:45:55 +0100

    initial commit

---
+++ JavaScriptAlgorithmsAndDataStructures/001-BasicJavaScript/036-WordBlanks.js
@@ -0,0 +1,8 @@
+var myNoun = "dog";
+var myAdjective = "big";
+var myVerb = "ran";
+var myAdverb = "quickly";
+
+// Only change code below this line
+var wordBlanks = "My " + myNoun + " is very " + myAdjective + " but yesterday at the park it " + myVerb + " very " + myAdverb; // Change this line
+// Only change code above this line

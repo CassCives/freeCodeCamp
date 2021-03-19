609487144d018ff0b3d766f8dc9615d5357bc2d9
CassCives <cass-cives@hotmail.com>
2021-03-19 20:19:19 +0100

    initial commit

---
+++ JavaScriptAlgorithmsAndDataStructures/001-BasicJavaScript/090-ManipulatingComplexObjects.js
@@ -0,0 +1,15 @@
+var myMusic = [
+	{
+		artist: "Billy Joel",
+		title: "Piano Man",
+		release_year: 1973,
+		formats: ["CD", "8T", "LP"],
+		gold: true,
+	},
+	{
+		artist: "This Cold Night",
+		title: "Each Other",
+		release_year: 2016,
+		formats: ["CD", "8T", "LP"],
+	},
+]
